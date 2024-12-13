b812ffb69014fa939504b662b17cf187b2659f0f
Yusuf260511 <sultvnhuzaevusufhuza@gmail.com>
2024-12-13 22:50:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let string = prompt('String typedagi malumotni kiriting')
+let stringItog = typeof string
+
+let number = prompt('Number typedagi malumotni kiriting')
+let numberItog = typeof number
+
+let boolean = prompt('Boolean typedagi malummotni kiriting')
+let booleanItog = typeof boolean
+
+alert('Hamma malumotni to`g`ri kiritganizga ishonchiz komilmi?')
+
+alert(`Siz kiritgan ${string} sozi ${stringItog} typega tegishli \n Siz kiritgan ${number} raqami ${numberItog} typega tegishli \n Siz kiritgan ${boolean} elementi ${booleanItog} typega tegishli`)
